f65b1004bbaeb30ecd294c44439d21107ef64380
hanielss <haniellemes@hotmail.com>
2023-09-13 16:51:00 -0300

    alteracao-de-valores-min-e-max-no-JavaScript

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-function generateNumber(){
-   
+function generateNumber() {
+
    const min = Math.ceil(document.querySelector(".input-min").value)
    const max = Math.floor(document.querySelector(".input-max").value)
 
 
    const result = Math.floor(Math.random() * (max - min + 1) + min);
+
    
-   alert(result)
-   
-   
-    
+
+
+   if (min >= max) {
+      alert("o valor mínimo NÃO pode ser maior que o máximo")
+   }
+
+   else {
+      const result = Math.floor(Math.random() * (max - min + 1) + min);
+      alert(result)
+   }
+
+
 }
